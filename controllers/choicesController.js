d1fc035cb14b78891b46b051bbddfb57a3763062
joostflick <joostflick@gmail.com>
2018-04-03 15:51:09 +0200

    choices working, starting listview of matches

--- controllers/choicesController.js
+++ controllers/choicesController.js
@@ -1,37 +1,21 @@
 var User = require('../models/user');
 
 function addChoices(req, res) {
-  console.log(req.body);
-  var option1 = req.body.select;
-  var option2 = req.body.select2;
-  var option3 = req.body.select3;
-  // determine choices based on a string of 1's and 0's
-  var newChoices = option1 + option2 + option3;
-  console.log(req.session.userId);
-  // check which user is logged in atm
-  User.findById(req.session.userId).exec(function(error, user) {
-      if (error) {
-        return next(error);
-      } else {
-        if (user === null) {
-          res.render('unauthorized');
-        } else {
-          console.log(newChoices);
-          // if user is logged in update his choices field with the choices of the form]
-          User.findOneAndUpdate({_id: user._id}, {$set: {choices: newChoices}}, function(err, doc) {
-            return res.status(200);
-            if (err) {
-              console.log("Something wrong when updating data!");
-            }
-            console.log(doc);
-          });
-        }
+  var newChoices = req.body.select + req.body.select2 + req.body.select3;
+  var id = req.session.userId;
+  if(!id){
+    res.render('unauthorized');
+  } else {
+  User.findOneAndUpdate({_id: id}, {$set: {choices: newChoices}}, function(err, doc) {
+    if (err) {
+      return res.sendStatus(400);
+    }
+    res.render('users.ejs', { "User": User });
 
-      }
-    });
-  return res.status(400);
+  });
+}
 }
 
 module.exports = {
-  addChoices: addChoices
+  addChoices: addChoices,
 };
